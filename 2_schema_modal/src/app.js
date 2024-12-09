c5056f2fa988336d08d6852098714e34d8e5616e
webOps-aman <amanhxe@gmail.com>
2024-12-09 20:29:11 +0530

    creating connection mongoose and create schema and modal

---
+++ 2_schema_modal/src/app.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+//connection creation and creating new db
+mongoose.connect("mongodb://127.0.0.1:27017/amanbase")
+    .then(() => console.log("Connected to MongoDB successfully!"))
+    .catch(err => console.error("Could not connect to MongoDB:", err));
+
+
