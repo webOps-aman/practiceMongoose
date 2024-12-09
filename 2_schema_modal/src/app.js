07161aa8a3e2139369557166ec8b993922726326
webOps-aman <amanhxe@gmail.com>
2024-12-09 20:37:30 +0530

    error fix

--- 2_schema_modal/src/app.js
+++ 2_schema_modal/src/app.js
@@ -5,4 +5,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/amanbase")
     .then(() => console.log("Connected to MongoDB successfully!"))
     .catch(err => console.error("Could not connect to MongoDB:", err));
 
+// //schema
+// const playlistSchema = new mongoose.Schema({
 
+//     name: String,
+//     ctype: String,
+//     videos: Number,
+//     author: String,
+//     active: Boolean,
+//     date: {
+//         type: Date,
+//         default: Date.now
+//     }
+// })
+// ___________
+
+
+//schema
+const playlistSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+//collection creation
+const Playlist = new mongoose.model("Playlist", playlistSchema);
